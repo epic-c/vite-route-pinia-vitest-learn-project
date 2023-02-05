7fd56ee31cf9d91db78abb049df4e96e8027f231
epic.chen <epicer.chen@gmail.com>
2023-02-05 23:36:38 +0800

    update

--- src/views/NoteView.vue
+++ src/views/NoteView.vue
@@ -2,8 +2,8 @@
   <div>note</div>
 
   <ul>
-    <li v-for="i in txts" :key="i">
-      {{ i }}
+    <li v-for="i in txts" :key="i.date">
+      {{ `${i.msg} ----- ${i.date}` }}
     </li>
     <input type="text" v-model="word" @keydown.enter="enter()" />
   </ul>
@@ -14,14 +14,22 @@ export default {
   methods: {
     enter() {
       if (this.word.length > 0) {
-        this.txts.push(this.word);
+        this.txts.push({
+          msg: this.word,
+          date: new Date(Date.now()).toLocaleString(),
+        });
         this.word = "";
       }
     },
   },
   data() {
     return {
-      txts: ["123", "456"],
+      txts: [
+        {
+          msg: "start",
+          date: new Date(Date.now()).toLocaleString(),
+        },
+      ],
       word: "",
     };
   },
