3cfec2d92c77d59d150829554d221b6e53700f32
Bruce-TC <115294354+Bruce-TC@users.noreply.github.com>
2023-02-05 22:56:13 +0800

    UPDATE1

--- src/views/NoteView.vue
+++ src/views/NoteView.vue
@@ -1,1 +1,29 @@
-<template><div>note</div></template>
+<template>
+  <div>note</div>
+
+  <ul>
+    <li v-for="i in txts" :key="i">
+      {{ i }}
+    </li>
+    <input type="text" v-model="word" @keydown.enter="enter()" />
+  </ul>
+</template>
+
+<script lang="ts">
+export default {
+  methods: {
+    enter() {
+      if (this.word.length > 0) {
+        this.txts.push(this.word);
+        this.word = "";
+      }
+    },
+  },
+  data() {
+    return {
+      txts: ["123", "456"],
+      word: "",
+    };
+  },
+};
+</script>
