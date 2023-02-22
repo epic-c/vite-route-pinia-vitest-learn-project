013cfa91e286deb871f9136ebda60e60883d6243
Bruce-TC <115294354+Bruce-TC@users.noreply.github.com>
2023-02-22 21:13:00 +0800

    update

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -18,54 +18,71 @@
     </thead>
     <tbody>
       <tr>
-        <td>sample</td>
+        <td v-for="(item, index) in calendar" :key="item.date.getDay()">
+          <input
+            type="text"
+            v-model="msg[index]"
+            @keydown.enter="enter(index)"
+          />
+          <div v-for="note in item.notes" :key="note.id">
+            {{ note.noteMsg }}
+          </div>
+        </td>
       </tr>
     </tbody>
   </table>
 </template>
-<script lang="ts">
-export default {
-  data() {
-    return {
-      calendar: [] as {
-        date: Date;
-        notes: { time: string; note: string }[];
-      }[],
-    };
-  },
-  methods: {
-    getDayName(day: number) {
-      switch (day) {
-        case 0:
-          return "星期日";
-        case 1:
-          return "星期一";
-        case 2:
-          return "星期二";
-        case 3:
-          return "星期三";
-        case 4:
-          return "星期四";
-        case 5:
-          return "星期五";
-        case 6:
-          return "星期六";
-        default:
-          return "解析錯誤";
-      }
-    },
-  },
-  created() {
-    for (let i = 0; i < 7; i++) {
-      const date = new Date();
-      date.setDate(date.getDate() - (date.getDay() - (i + 1)));
-      this.calendar.push({
-        date,
-        notes: [],
-      });
-    }
-  },
-};
+<script setup lang="ts">
+import { ref } from "vue";
+import { v4 } from "uuid";
+
+interface Note {
+  time: string;
+  noteMsg: string;
+  id: string;
+}
+
+const calendar = ref<{ date: Date; notes: Note[] }[]>([]);
+const msg = ref<string[]>([]);
+function getDayName(day: number) {
+  switch (day) {
+    case 0:
+      return "星期日";
+    case 1:
+      return "星期一";
+    case 2:
+      return "星期二";
+    case 3:
+      return "星期三";
+    case 4:
+      return "星期四";
+    case 5:
+      return "星期五";
+    case 6:
+      return "星期六";
+    default:
+      return "解析錯誤";
+  }
+}
+function enter(index: number) {
+  if (msg.value[index].length > 0) {
+    calendar.value[index].notes.push({
+      time: new Date(Date.now()).toLocaleString(),
+      noteMsg: msg.value[index],
+      id: v4(),
+    });
+    msg.value[index] = "";
+  }
+}
+for (let i = 0; i < 7; i++) {
+  const date = new Date();
+  date.setDate(date.getDate() - (date.getDay() - (i + 1)));
+  calendar.value.push({
+    date,
+    notes: [] as Note[],
+  });
+  msg.value.push("");
+}
 </script>
 
 <style>
